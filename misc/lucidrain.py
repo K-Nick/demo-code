ac0f648b79bee9a08bd215996de9ff86954f0e7a
NickyMouse <kningtg@gmail.com>
2023-12-15 10:47:31 +0800

    Update misc/lucidrain.py

---
+++ misc/lucidrain.py
@@ -0,0 +1,7 @@
+def group_by_key_prefix(prefix, d):
+    return group_dict_by_key(partial(string_begins_with, prefix), d)
+
+def groupby_prefix_and_trim(prefix, d):
+    kwargs_with_prefix, kwargs = group_dict_by_key(partial(string_begins_with, prefix), d)
+    kwargs_without_prefix = dict(map(lambda x: (x[0][len(prefix):], x[1]), tuple(kwargs_with_prefix.items())))
+    return kwargs_without_prefix, kwargs
